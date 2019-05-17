ed0097fd7eabba40606e7377dd503fc0b7b77172
Angelina Weibert <awb@angelinas-mbp.eduroam.hs-furtwangen.de>
2019-05-17 15:18:49 +0200

    type

--- Typescript/script.ts
+++ Typescript/script.ts
@@ -1,2 +1,18 @@
 console.log("Test1");
 // Dies ist ein Kommentar
+
+window.onload = function () {
+    console.log("Objekte wurden geladen");
+    document.getElementById("id1").addEventListener("click", DoStuff1);
+    document.getElementById("erbwb").addEventListener("click", DoStuff2);
+}
+function DoStuff1()
+{
+    console.log("clicked on button 1");
+    
+}
+
+function DoStuff2()
+{
+    console.log("button 2 was clicked");
+}
