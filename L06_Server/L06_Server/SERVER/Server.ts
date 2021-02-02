be8ccfd213b5754ab511f456ca9b2c562a2485ba
weiberta <48830856+weiberta@users.noreply.github.com>
2021-02-02 23:12:00 +0100

    Add files via upload

---
+++ L06_Server/L06_Server/SERVER/Server.ts
@@ -0,0 +1,42 @@
+import * as Http from "http";
+import * as Url from "url";
+
+// SERVER
+export namespace Coronahilfe {
+    let server: Http.Server = Http.createServer();
+    console.log(server);
+
+    //
+    let port: number | string | undefined  = process.env.PORT;
+    if (port == undefined)
+        port = 5001;
+
+    console.log("Server starting on port: " + port);
+
+    server.listen(port);
+    server.addListener("request", handleRequest);
+
+
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+
+        console.log("MROW.");
+        console.log(_request.url);
+
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*"); //sicherheitsmechanismus (Browser) wird ausgeschaltet
+
+        if (_request.url) {
+            let url: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+
+            for (let key in url.query) {
+                _response.write(key + ":  " + url.query[key] + "\n"); // kann reinschreiben was ich will, auch umbrüche
+            }
+
+            let jsonString: string = JSON.stringify(url.query);
+            _response.write(jsonString);
+        }
+
+        _response.end();
+
+    }
+}
